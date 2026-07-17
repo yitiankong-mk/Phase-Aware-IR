5906aea3a78b349e6c405e62c0e23141539667d1
yitiankong-mk <240801314+yitiankong-mk@users.noreply.github.com>
2026-07-17 03:58:33 -0700

    Remove AggMapNet and IRCNN models

--- models/__init__.py
+++ models/__init__.py
@@ -1,24 +1,20 @@
 """Model architectures used in the Phase-Aware-IR experiments."""
 
-from .aggmapnet import AggMapNetPaperLike
 from .dualhead import DualHeadCNN
 from .dualhead_double_channels import DualHeadDoubleChannelsCNN
 from .dualhead_double_channels_wide_mlp import DualHeadDoubleChannelsWideMLPCNN
 from .dualhead_extra_final_conv import DualHeadExtraFinalConvCNN
 from .dualhead_wide_mlp import DualHeadCNNMLP2048
-from .ircnn import IRCNNPaper
 from .patch_transformer import IRPatchTransformerCNN
 from .resnet50_1d_base64 import ResNet50_1DBase64CNN
 from .resnet50_1d_base128 import ResNet50_1DCNN
 
 __all__ = [
-    "AggMapNetPaperLike",
     "DualHeadCNN",
     "DualHeadCNNMLP2048",
     "DualHeadDoubleChannelsCNN",
     "DualHeadDoubleChannelsWideMLPCNN",
     "DualHeadExtraFinalConvCNN",
-    "IRCNNPaper",
     "IRPatchTransformerCNN",
     "ResNet50_1DBase64CNN",
     "ResNet50_1DCNN",
